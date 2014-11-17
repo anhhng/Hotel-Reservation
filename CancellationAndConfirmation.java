21b40c6101ac74242d4405983f0c91073adf6bf3
danieltn91 <danieltn91@gmail.com>
2014-11-17 14:29:23 -0800

    Create CancellationAndConfirmation.java

---
+++ CancellationAndConfirmation.java
@@ -0,0 +1,67 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+
+public class CancellationAndConfirmation extends JFrame
+{
+	public static void main(String[] arg)
+	{
+		new CancellationAndConfirmation();
+	}
+	
+	public CancellationAndConfirmation()
+	{
+		CancelAndConfirm();
+	}
+	public void CancelAndConfirm()
+	{
+		setSize(50,50);
+		JPanel panel = new JPanel();
+		
+		JLabel label = new JLabel("Click Confirm to confirm your reservation or "
+				+ "	click Cancel to cancel your resveration", SwingConstants.CENTER);
+		JButton cancel = new JButton("Cancel");
+	
+		JButton confirm = new JButton("Confirm");
+	
+		panel.add(label);
+		panel.add(confirm);
+		panel.add(cancel);
+		cancel.addActionListener(new ActionListener() 
+		{ 
+			@Override 
+			public void actionPerformed(ActionEvent arg0)
+			{ 
+				//add our code to cancel reservation
+				System.out.println("Cancelled");
+			
+			} 
+		}); 
+		confirm.addActionListener(new ActionListener() 
+		{ 
+			@Override 
+			public void actionPerformed(ActionEvent arg0)
+			{ 
+				//add our code here to make a confirmation
+				System.out.println("Confirmed");
+			
+			} 
+		}); 
+	
+	
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE ); 
+		frame.setLayout(new BorderLayout());
+		frame.add(panel);
+		frame.pack();
+		frame.setVisible(true);
+	}
+}
