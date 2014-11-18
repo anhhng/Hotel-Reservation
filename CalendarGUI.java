2d3bbdeb7b596a109a1e51acac44c8de5b07e6c2
David Delgado <alohakealii@gmail.com>
2014-11-17 16:28:44 -0800

    calendargui for daniel

--- CalendarGUI.java
+++ CalendarGUI.java
@@ -1,5 +1,8 @@
 package projecttester;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
 import java.text.DateFormatSymbols;
 import java.util.*;
 import javax.swing.*;
@@ -14,5 +17,28 @@ public class CalendarGUI extends JPanel
       JComboBox months = new JComboBox(new DateFormatSymbols().getMonths());
       JSpinner year = new JSpinner();
       year.setValue(cal.get(Calendar.YEAR));
+      setLayout(new BorderLayout());
+      JPanel header = new JPanel();
+      header.add(months);
+      header.add(year);
+      
+      JPanel calendar = new JPanel();
+      JLabel weekdays = new JLabel("Su  Mo  Tu  We  Th  Fr  Sa");
+      weekdays.setForeground(Color.blue);
+      calendar.add(weekdays);
+      calendar.setLayout(new GridLayout(7,6));
+      int count = 1;
+      while (count < cal.get(GregorianCalendar.DAY_OF_WEEK))
+      {
+         JLabel label = new JLabel();
+         calendar.add(label);
+         count++;
+      }
+      for (int i = 1; i <= cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH); i++)
+      {
+         JLabel label = new JLabel("" + i);
+         calendar.add(label);
+      }
+      
    }
 }
