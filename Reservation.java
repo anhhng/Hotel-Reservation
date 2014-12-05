c93ed46c8832cad07175d6e1e6d8ae0d1205fad8
David Delgado <alohakealii@gmail.com>
2014-12-05 11:24:52 -0800

    javadoc for Reservation

--- Reservation.java
+++ Reservation.java
@@ -1,3 +1,8 @@
+/**
+Reservation class for Group Project
+@author David Delgado, Daniel Nguyen, Anh Nguyen
+*/
+
 package projecttester;
 
 import java.io.Serializable;
@@ -5,6 +10,9 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
+/**
+Reservation contains information about a reservation
+*/
 public class Reservation implements Serializable
 {
    private static int nextReservationNumber = 1500;
@@ -17,20 +25,50 @@ public class Reservation implements Serializable
    private double costPerDay;
    private int numberOfDays;
    
-   public Reservation(GregorianCalendar start, GregorianCalendar end, int accountID,
-                      int room, double aCostPerDay)
+   /**
+   Constructs Reservation object
+   @param start date reservation starts
+   @param end date reservation ends
+   @param accountID account ID of guest
+   @param room room number
+   @param aCostPerDay cost of room per night
+   */
+   public Reservation(GregorianCalendar start, GregorianCalendar end, 
+           int accountID, int room, double aCostPerDay)
    {
-      initReservation(start, end, accountID, room, aCostPerDay, nextReservationNumber++);
+      initReservation(start, end, accountID, room, aCostPerDay, 
+              nextReservationNumber++);
    }
    
-   public Reservation(GregorianCalendar start, GregorianCalendar end, int accountID,
-                      int room, double aCostPerDay, int aReservationNumber)
+   /**
+   Constructs Reservation object with manually set reservation number
+   @param start date reservation starts
+   @param end date reservation ends
+   @param accountID account ID of guest
+   @param room room number
+   @param aCostPerDay cost of room per night
+   @param aReservationNumber reservation number
+   */
+   public Reservation(GregorianCalendar start, GregorianCalendar end, 
+           int accountID, int room, double aCostPerDay, int aReservationNumber)
    {
-      initReservation(start, end, accountID, room, aCostPerDay, aReservationNumber);
+      initReservation(start, end, accountID, room, aCostPerDay, 
+              aReservationNumber);
    }
    
+   /**
+   Sets the information for a new Reservation
+   @param start date reservation starts
+   @param end date reservation ends
+   @param accountID account ID of guest
+   @param room room number
+   @param aCostPerDay cost of room per night
+   @param aNumber reservation number
+   postcondition: the new reservation has the given data
+   */
    private void initReservation(GregorianCalendar start, GregorianCalendar end, 
-                           int accountID, int room, double aCostPerDay, int aNumber)
+                           int accountID, int room, double aCostPerDay, 
+                           int aNumber)
    {
       startDate = start;
       endDate = end;
@@ -39,52 +77,90 @@ public class Reservation implements Serializable
       reservationNumber = aNumber;
       costPerDay = aCostPerDay;
       if (startDate.get(Calendar.YEAR) == endDate.get(Calendar.YEAR))
-         numberOfDays = endDate.get(Calendar.DAY_OF_YEAR) - startDate.get(Calendar.DAY_OF_YEAR);
+         numberOfDays = endDate.get(Calendar.DAY_OF_YEAR) - 
+                 startDate.get(Calendar.DAY_OF_YEAR);
       else
-         numberOfDays = 365 - startDate.get(Calendar.DAY_OF_YEAR) + endDate.get(Calendar.DAY_OF_YEAR);
+         numberOfDays = 365 - startDate.get(Calendar.DAY_OF_YEAR) + 
+                 endDate.get(Calendar.DAY_OF_YEAR);
       cost = costPerDay * (double)(numberOfDays);
    }
    
+   /**
+   Gets the duration of the reservation
+   @return number of days
+   */
    public int getNumberOfDays()
    {
       return numberOfDays;
    }
    
+   /**
+   Gets the account ID of the guest associated with the reservation
+   @return account ID
+   */
    public int getAcctID()
    {
       return acctID;
    }
 
+   /**
+   Gets the date the reservation starts
+   @return start date
+   */
    public GregorianCalendar getArrivalDate()
    {
       return startDate;
    }
    
+   /**
+   Gets the date the reservation ends
+   @return end date
+   */
    public GregorianCalendar getDepartDate()
    {
       return endDate;
    }
    
+   /**
+   Gets the reservation number
+   @return reservation number
+   */
    public int getReservationNumber()
    {
       return reservationNumber;
    }
    
+   /**
+   Gets the number of the room reserved
+   @return room number
+   */
    public int getRoomNumber()
    {
       return roomNumber;
    }
    
+   /**
+   Gets the total cost of the reservation
+   @return total cost
+   */
    public double getCost()
    {
       return cost;
    }
    
+   /**
+   Gets the nightly cost of the room reserved
+   @return cost per day
+   */
    public double getCostPerDay()
    {
       return costPerDay;
    }
    
+   /**
+   Gets a string representation of the start date
+   @return start date string
+   */
    public String printStartDate()
    {
       SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy"); 
@@ -92,6 +168,10 @@ public class Reservation implements Serializable
       return format.format(startDate.getTime());
    }
    
+   /**
+   Gets a string representation of the end date
+   @return end date string
+   */
    public String printEndDate()
    {
       SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy"); 
@@ -99,11 +179,22 @@ public class Reservation implements Serializable
       return format.format(endDate.getTime());
    }
    
-   public static void setNextReserverationNumber(int aNumber) {
-       nextReservationNumber = aNumber;
+   /**
+   Sets the next reservation number in the system
+   @param aNumber next reservation number
+   postcondition: next reservation number is set to aNumber
+   */
+   public static void setNextReserverationNumber(int aNumber) 
+   {
+      nextReservationNumber = aNumber;
    }
    
-   public static int getNextReserverationNumber() {
-       return nextReservationNumber;
+   /**
+   Gets the next reservation number in the system
+   @return next reservation number
+   */
+   public static int getNextReserverationNumber() 
+   {
+      return nextReservationNumber;
    }
 }
